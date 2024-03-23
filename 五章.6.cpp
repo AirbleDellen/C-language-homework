097f5b7f667e7f3f77f4da945c278dbb33c80ca1
AirbleDellen <145096853+AirbleDellen@users.noreply.github.com>
2024-03-23 09:57:00 +0800

    Update 五章.6.cpp

--- 五章.6.cpp
+++ 五章.6.cpp
@@ -3,9 +3,10 @@ int  main()
 {
 	int i=1,n=1;
 	long long sum=0,c1=0;
+	//注意，此处将c1,sum都设置成longlong，防止过程中c1与sum溢出
 	for(;i<=20;i++)
-	{c1=1;
-	 n=1;
+	{c1=1;//循环开始重置c1
+	 n=1;//循环开始重置n
 		while(n<=i)
 		{
 		  c1=c1*n;
